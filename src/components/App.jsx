362cf9413efa24178309d2d90a8c3f198c835cf2
Josh Cobert <jdcobert@gmail.com>
2022-04-17 21:26:59 -0400

    create card framework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,28 @@
 import '../css/App.css';
+import people from "../people.json";
+
+let peopleCards = [];
+for (let i = 0; i < people.length; i++) {
+  let p = i;
+  if (people[i].hasOwnProperty("partner")) {
+    p = people[i].partner - 1;
+  }
+  peopleCards.push(
+    <Card
+      key={people[i].id}
+      id={people[i].id}
+      name={people[i].name}
+      img={people[i].img}
+      loc={people[i].loc}
+      assoc={people[i].assoc}
+      rel={people[i].rel}
+      partner={people[p]}
+      nicknames={people[i].nicknames}
+      dob={people[i].dob}
+    />
+  );
+}
+
 
 function App() {
   return (

6ac6e6763a494e0603eba1e8c771b36c956a458b
Josh Cobert <jdcobert@gmail.com>
2022-05-23 17:33:03 -0400

    edit tagline

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -6,7 +6,7 @@ function Heading() {
         <>
             <div className='text-center md:text-left md:px-4 mb-12 md:mb-0'>
                 <h2 className='text-5xl font-life-savers font-extrabold text-[#2B4E6A] mb-2 md:mb-4'>Who's That?</h2>
-                <p className='px-8 md:px-0'>An encyclopedia of the people in your life. And a cheat sheet for your forgetful partner.</p>
+                <p className='px-8 md:px-0'>An encyclopedia of the people in your life. And a cheat sheet for your partner who can't keep track.</p>
             </div>
         </>
     );

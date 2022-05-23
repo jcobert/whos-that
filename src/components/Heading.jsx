fb871eb89c3b200842135998e6ff934536fc409c
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2022-05-23 17:38:33 -0400

    Add landing heading (#9)
    * add content to and style main heading
    
    * edit tagline
    
    * edit tagline

---
+++ src/components/Heading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Heading() {
+
+    return (
+        <>
+            <div className='text-center md:text-left md:px-4 mb-12 md:mb-0'>
+                <h2 className='text-5xl font-life-savers font-extrabold text-[#2B4E6A] mb-2 md:mb-4'>Who's That?</h2>
+                <p className='px-8 md:px-0'>An encyclopedia of the people in your life. And a cheat sheet for those who can't keep track.</p>
+            </div>
+        </>
+    );
+}
+
+export default Heading;

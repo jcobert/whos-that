d7928efed99ff4ad1661f6e66837908d206db2eb
Josh Cobert <jdcobert@gmail.com>
2022-05-24 21:34:40 -0400

    add navlinks to mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,7 +56,7 @@ function Header(props) {
                 className={`bg-slate-500 fixed w-full z-50 top-0 shadow-md mb-16`}
                 onClick={setVisible}
             >
-                {({ open }) => (
+                {({ close }) => (
                     <>
                         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                             <div className={`relative flex items-center justify-between h-16 ${visible ? "h-16" : "h-10"} transition-all`}>
@@ -64,7 +64,7 @@ function Header(props) {
                                     {/* Mobile menu button*/}
                                     <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none">
                                         <span className="sr-only">Open main menu</span>
-                                        {open ? (
+                                        {!close ? (
                                             <XIcon className="block h-6 w-6" aria-hidden="true" />
                                         ) : (
                                             <MenuIcon className="block h-6 w-6" aria-hidden="true" />
@@ -90,7 +90,7 @@ function Header(props) {
                                                 <NavLink
                                                     key={item.name}
                                                     to={item.to}
-                                                    className={({isActive}) => (
+                                                    className={({ isActive }) => (
                                                         `${isActive ? 'text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'} px-3 py-2 rounded-md text-sm font-medium`
                                                     )}
                                                     aria-current={item.current ? 'page' : undefined}
@@ -170,24 +170,23 @@ function Header(props) {
                         <Disclosure.Panel className="sm:hidden">
                             <div className="px-2 pt-2 pb-3 space-y-1">
                                 {navigation.map((item) => (
-                                    <Disclosure.Button
+                                    <NavLink
                                         key={item.name}
-                                        as="a"
-                                        href={item.href}
-                                        className={classNames(
-                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                            'block px-3 py-2 rounded-md text-base font-medium text-right'
+                                        to={item.to}
+                                        className={({ isActive }) => (
+                                            `${isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'} block px-3 py-2 rounded-md text-base font-medium text-right`
                                         )}
                                         aria-current={item.current ? 'page' : undefined}
+                                        onClick={close}
                                     >
                                         {item.name}
-                                    </Disclosure.Button>
+                                    </NavLink>
                                 ))}
                             </div>
                         </Disclosure.Panel>
                     </>
                 )}
-            </Disclosure>            
+            </Disclosure>
         </div>
     );
 }

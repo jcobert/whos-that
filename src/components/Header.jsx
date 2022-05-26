084c10e757da922872750ba9b6d9c54c8cafc119
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2022-05-25 21:42:16 -0400

    assign header logo unique keys (#12)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -79,7 +79,7 @@ function Header(props) {
                                 <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                                     <div className="flex-shrink-0 flex items-center">
                                         <NavLink
-                                            key={'logoHome'}
+                                            key={'logoHomeSm'}
                                             to={'/'}
                                         >
                                             <img
@@ -89,7 +89,7 @@ function Header(props) {
                                             />
                                         </NavLink>
                                         <NavLink
-                                            key={'logoHome'}
+                                            key={'logoHomeLg'}
                                             to={'/'}
                                         >
                                             <img

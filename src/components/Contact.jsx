82e67a4db90742e84e548218fbb11fda28968dd5
Josh Cobert <jdcobert@gmail.com>
2022-06-19 01:29:35 -0400

    add email cta

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { MailIcon } from "@heroicons/react/outline";
 
 function Contact(props) {
-
-
-    return (
-        <div className="mt-16">
-            <h1>Contact</h1>
+  return (
+    <div className="mt-16">
+      <div className="mt-6 text-center space-y-4">
+        <h1 className="text-4xl font-life-savers font-extrabold text-[#2B4E6A]">
+          Contact
+        </h1>
+        <p>Questions? Comments? Concerns?</p>
+      </div>
+      <div className="text-center mt-12">
+        <div className="w-7/12 mx-auto text-sky-800 text-xl font-medium bg-slate-50 border border-gray-300 rounded-md shadow-md p-2">
+          <div className="flex flex-row justify-center space-x-2 p-2">
+              <MailIcon className="w-7" />
+              <a className="" href="mailto:josh@whosthat.io">
+                Drop us a line!
+              </a>
+          </div>
         </div>
-    );
+        <p className="mt-12">We value and appreciate your feedback.</p>
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default Contact;

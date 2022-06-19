5fbaa4dfc916afb63d476a500d02b7fdac43c285
Josh Cobert <jdcobert@gmail.com>
2022-06-19 15:36:14 -0400

    improve styling

--- src/components/Careers.jsx
+++ src/components/Careers.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { BriefcaseIcon } from "@heroicons/react/outline";
 
 function Careers(props) {
   return (
     <div className="mt-16">
-      <div className="mt-6 sm:mt-12 text-center space-y-4 px-8">
+      <div className="mt-6 sm:mt-12 text-center px-8">
         <h1 className="text-4xl font-life-savers font-extrabold text-[#2B4E6A]">
           Careers
         </h1>
-        <p>
-          We're not hiring at the moment but if you are, please feel free to{" "}
-          <a
-            className="font-medium text-sky-800 hover:text-sky-600 transition-all"
-            href="mailto:josh@joshcobert.com"
-          >
-            reach out!
-          </a>
-        </p>
+        <div className="mt-8 md:mt-14 space-y-8 md:space-y-14">
+          <BriefcaseIcon className="w-12 mx-auto text-slate-600" />
+          <p className="">
+            We're not hiring at the moment but if you are, please feel free to{" "}
+            <a
+              className="font-medium text-sky-800 hover:text-sky-600 transition-all"
+              href="mailto:josh@joshcobert.com"
+            >
+              reach out!
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );

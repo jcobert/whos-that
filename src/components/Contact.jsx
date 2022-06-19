e1215f401a2657a0b365a3917336b7ec08283888
Josh Cobert <jdcobert@gmail.com>
2022-06-19 12:06:15 -0400

    style responsive layouts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,24 +4,23 @@ import { MailIcon } from "@heroicons/react/outline";
 function Contact(props) {
   return (
     <div className="mt-16">
-      <div className="mt-6 text-center space-y-4">
+      <div className="mt-6 sm:mt-12 text-center space-y-4">
         <h1 className="text-4xl font-life-savers font-extrabold text-[#2B4E6A]">
           Contact
         </h1>
         <p>Questions? Comments? Concerns?</p>
       </div>
       <div className="text-center mt-12">
-        <div className="w-7/12 mx-auto text-sky-800 text-xl font-medium bg-slate-50 border border-gray-300 rounded-md shadow-md p-2">
-          <div className="flex flex-row justify-center space-x-2 p-2">
+        <a className="inline-block w-7/12 sm:w-60" href="mailto:josh@whosthat.io">
+          <div className="mx-auto text-sky-800 hover:text-sky-700 text-xl md:text-lg font-medium bg-slate-50 hover:bg-slate-100 active:bg-slate-200 transition-all border border-gray-300 rounded-md shadow-md p-2 md:p-1">
+            <div className="flex flex-row justify-center space-x-2 p-2">
               <MailIcon className="w-7" />
-              <a className="" href="mailto:josh@whosthat.io">
-                Drop us a line!
-              </a>
+              <p>Drop us a line!</p>
+            </div>
           </div>
-        </div>
+        </a>
         <p className="mt-12">We value and appreciate your feedback.</p>
       </div>
-      <div></div>
     </div>
   );
 }

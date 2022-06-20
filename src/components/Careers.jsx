d63dcafd8c4fe9302e2f59c34e2868c446f0b953
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2022-06-19 23:04:33 -0400

    Build careers page (#20)
    * add cta blurb
    
    * improve styling
    
    * adjust content spacing
    
    * remove uneeded markup

--- src/components/Careers.jsx
+++ src/components/Careers.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { BriefcaseIcon } from "@heroicons/react/outline";
 
 function Careers(props) {
-
-
-    return (
-        <div className="mt-16">
-            <h1>Careers</h1>
+  return (
+    <div className="mt-16">
+      <div className="mt-6 sm:mt-12 text-center px-8">
+        <h1 className="text-4xl font-life-savers font-extrabold text-[#2B4E6A]">
+          Careers
+        </h1>
+        <div className="mt-8 md:mt-14 space-y-8 md:space-y-14">
+          <BriefcaseIcon className="w-12 md:w-16 mx-auto text-slate-600" />
+          <p className="">
+            We're not hiring at the moment but if you are, please feel free to{" "}
+            <a
+              className="font-medium text-sky-800 hover:text-sky-600 transition-all"
+              href="mailto:josh@joshcobert.com"
+            >
+              reach out!
+            </a>
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Careers;
